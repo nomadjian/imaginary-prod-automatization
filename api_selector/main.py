f30d70f2c4c73769d4d9c7215e92fd51ea45990e
nomadjian <wernerss929@gmail.com>
2023-06-25 16:34:10 +0500

    feat(api-selector): add auth middleware and login proto

--- api_selector/main.py
+++ api_selector/main.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+from permission_middleware import auth_middleware
 
 routes = web.RouteTableDef()
 
@@ -6,6 +7,11 @@ routes = web.RouteTableDef()
 async def hello(request):
     return web.Response(text='Ello, GuvNor')
 
+@routes.get('/login')
+async def login(request):
+    return web.Response(text='You logined')
+
 app = web.Application()
+app.middlewares.append(auth_middleware)
 app.add_routes(routes)
 web.run_app(app)
