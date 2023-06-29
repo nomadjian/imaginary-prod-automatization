873a78e6f55617d1da9896d5cbf74456035b6b2d
nomadjian <wernerss929@gmail.com>
2023-06-29 16:41:59 +0500

    refact(api_selector): auth and user logic replaced from api_selector

--- api_selector/tune_alembic.sh
+++
@@ -1,6 +0,0 @@
-sed -i 's/DB_USER/'"$POSTGRES_USER"'/g' alembic.ini
-sed -i 's/DB_PASS/'"$POSTGRES_PASSWORD"'/g' alembic.ini
-sed -i 's/DB_NAME/'"$POSTGRES_DB"'/g' alembic.ini
-sed -i 's/DB_PORT/'"$POSTGRES_PORT"'/g' alembic.ini
-sed -i 's/DB_URL/'"$POSTGRES_URL"'/g' alembic.ini
-exec "$@"
