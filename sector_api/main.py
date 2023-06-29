9895dac47a97c370d283c83fcd0fd0fba59ae1f8
nomadjian <wernerss929@gmail.com>
2023-06-29 17:53:55 +0500

    feat(proj): wrapped apis to docker

---
+++ sector_api/main.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+
+
+import json
+routes = web.RouteTableDef()
+
+@routes.get('/api/sector/avaliable_urls')
+async def get_paths(request):
+    data = [{"path":route.path, "method":route.method} for route in routes]
+    return web.Response(body=json.dumps(data), status=200)
+
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app, port=8081)
